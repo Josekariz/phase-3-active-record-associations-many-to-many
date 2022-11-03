1072efdef53ddd75f307c935bad6b8bdfb97a7ac
Josekariz <joseph.macharia@student.moringaschool.com>
2022-11-03 19:39:13 +0300

    Done

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base
-  has_many :reviews
+  has_many(:reviews)
+  has_many(:users, through: :reviews)
 end
+# There are a couple important things to note when using the has_many macro with the through: option. Order matters — you must place the first has_many that references the join table above the second has_many that goes through that join table. This code won't work:
